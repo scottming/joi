1e758e31368615eb46e79a3c017c1e3321d0862d
scottming <therealscottming@gmail.com>
2020-12-01 10:52:47 +0800

    Add validate_all_fields

--- lib/joi.ex
+++ lib/joi.ex
@@ -5,15 +5,40 @@ defmodule Joi do
 
   alias Joi.Type
 
+  # def validate(data, schema) do
+  #   Enum.reduce_while(schema, {:ok, data}, fn {field, [type | options]}, {:ok, modified_data} ->
+  #     case Type.validate(type, field, modified_data, options) do
+  #       {:error, msg} ->
+  #         {:halt, {:error, msg}}
+
+  #       {:ok, new_data} ->
+  #         {:cont, {:ok, new_data}}
+  #     end
+  #   end)
+  # end
+
   def validate(data, schema) do
-    Enum.reduce_while(schema, {:ok, data}, fn {field, [type | options]}, {:ok, modified_data} ->
+      data |> Map.put(:joi_errors, []) |> validate_all_fields(schema) |> parse_result()
+  end
+
+  def validate_all_fields(data, schema) do
+    Enum.reduce(schema, {:ok, data}, fn {field, [type | options]}, {:ok, modified_data} ->
       case Type.validate(type, field, modified_data, options) do
-        {:error, msg} ->
-          {:halt, {:error, msg}}
+        {:error, error} ->
+          {:ok, %{modified_data | joi_errors: [error | modified_data.joi_errors]}}
 
         {:ok, new_data} ->
-          {:cont, {:ok, new_data}}
+          {:ok, new_data}
       end
     end)
+  end
+
+  def parse_result(result) do
+    {:ok, data} = result
+
+    case data.joi_errors do
+      [] -> {:ok, data}
+      errors -> {:error, errors}
+    end
   end
 end
