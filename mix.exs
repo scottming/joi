c49409ad90b5f71118f1df472f8e57068f195771
scottming <therealscottming@gmail.com>
2020-12-05 21:19:05 +0800

    Modify release tag

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Joi.MixProject do
   def project do
     [
       app: :joi,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
