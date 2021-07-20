ba3040cff37d40862e8cff016624049d9673fb89
scottming <therealscottming@gmail.com>
2021-07-20 11:38:51 +0800

    Publish a version before breaking change

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Joi.MixProject do
   def project do
     [
       app: :joi,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
